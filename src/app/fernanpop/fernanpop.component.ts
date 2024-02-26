d491a8d95c00483f14e6e5e1ecd0d52af03f09bf
Jose Miguel Aranda Fernandez <josemi.arandafernandez@gmail.com>
2024-02-26 23:37:31 +0100

    styled searcher(unfinshed): added

--- src/app/fernanpop/fernanpop.component.ts
+++ src/app/fernanpop/fernanpop.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SearcherComponent } from '../shared/searcher/searcher.component';
 
 @Component({
   selector: 'app-fernanpop',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SearcherComponent],
   templateUrl: './fernanpop.component.html',
 })
 export class FernanpopComponent {
--- src/app/shared/searcher/searcher.component.ts
+++ src/app/shared/searcher/searcher.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostBinding, signal } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-searcher',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './searcher.component.html',
   styles: ``
 })
 export class SearcherComponent {
+  private cont = -1;
+  private products: string[] = ['nintendo', 'polystation', 'odoo premium', 'un café'];
+  title = 'auth-firebase';
+  darkMode = signal<boolean>(JSON.parse(localStorage.getItem('dark_mode') ?? 'false'));
+  animate = signal<boolean>(true);
+  buy = 'Compra';
+  product = signal<string>('un café');
+  messageActivate = signal<boolean>(true);
+  text = '';
 
+  constructor() {
+    interval(2000).subscribe(() => this.animate.set(!this.animate()));
+
+    interval(4000).subscribe(() => {
+      this.cont = (this.cont + 1) % this.products.length;
+      this.product.set(this.products[this.cont]);
+    });
+  }
+
+  @HostBinding('class.dark') get mode() {
+    return this.darkMode();
+  }
+
+  changeTheme() {
+    this.darkMode.set(!this.darkMode());
+    localStorage.setItem('dark_mode', `${this.darkMode()}`);
+  }
+
+  changeMessage() {
+    if (this.text === '') {
+      this.messageActivate.set(!this.messageActivate());
+    }
+  }
+
+  onInput(value: string) {
+    this.text = value;
+  }
 }
