e26837473eca63948c348bdb31788b11a19c2c0b
Jose Miguel <josemi.arandafernandez@gmail.com>
2025-02-19 17:05:26 +0100

    add state in update product component

--- src/app/fernanpop/pages/products/update-product/update-product.component.ts
+++ src/app/fernanpop/pages/products/update-product/update-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, signal } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, signal } from '@angular/core';
 import { Product } from '../../../../interfaces/product.interface';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductsService } from '../../../../services/products.service';
@@ -8,22 +8,33 @@ import { CommonModule } from '@angular/common';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { ErrorState, InitialState, LoadingState, State, SuccessState } from '../../../../states/state.interface';
+import { Subscription } from 'rxjs';
+import { CustomResponse, ErrorResponse, SuccessResponse } from '../../../../interfaces/response-interface';
+import { ProductButtonComponent } from '../../../components/product-button/product-button.component';
 
 @Component({
   selector: 'app-update-product',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ConfirmDialogModule, ButtonModule],
+  imports: [CommonModule, ReactiveFormsModule, ConfirmDialogModule, ButtonModule, ProductButtonComponent],
   templateUrl: './update-product.component.html',
   styleUrl: './update-product.component.css',
   providers: [ConfirmationService, MessageService]
 })
-export class UpdateProductComponent implements OnInit {
+export class UpdateProductComponent implements OnInit, OnDestroy {
   @Input('id') productId: string | undefined;
   public minLenght = 6;
   public maxLenght = 50;
   public maxDescLenght = 300;
   private currentUser = this.authService.currentUser;
-  public product = signal<Product | undefined>(undefined);
+  public productState = signal<State>(new LoadingState());
+  public updateProductState = signal<State>(new InitialState());
+  public deleteProductState = signal<State>(new InitialState());
+  private getProductsByIdSubscription: Subscription = new Subscription();
+  private updateProductSubscription: Subscription = new Subscription();
+  private deleteProductSubscription: Subscription = new Subscription();
+  public submitted = false;
+  public isLoading = false;
 
   form: FormGroup = new FormGroup({
     title: new FormControl(null),
@@ -32,32 +43,32 @@ export class UpdateProductComponent implements OnInit {
     desc: new FormControl(null),
   });
 
-  submitted = false;
 
   constructor(private formBuilder: FormBuilder, private productsService: ProductsService,
     private confirmationService: ConfirmationService, 
     private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-    // this.productsService.getProductById(this.productId!).subscribe((result: Product | null) => {
-    //   if (!result || result.sellerId !== this.currentUser()!.uid) {
-    //     // Redirección a error con mensaje
-    //     this.router.navigate(['fernanpop/error/'], {
-    //       state: {
-    //         message: 'Parece que el producto no se encuentra'
-    //       }
-    //     });
-    //   } else {
-    //     this.product.set(result);
-    //     // Cargamos en el formulario los datos del producto
-    //     this.form.patchValue({
-    //       title: this.product()!.title,
-    //       price: this.product()!.price,
-    //       img: this.product()!.img,
-    //       desc: this.product()!.desc
-    //     });
-    //   }
-    // });
+    this.getProductsByIdSubscription = this.productsService.getProductById(this.productId!).subscribe({
+      next: (response: CustomResponse) => {
+        if (response instanceof SuccessResponse) {
+          this.productState.set(new SuccessState(response.data));
+          const { title, price, img, desc } = response.data;
+          this.form.patchValue({
+            title: title,
+            price: price,
+            img: img,
+            desc: desc
+          });
+        } else if (response instanceof ErrorResponse) {
+          this.router.navigate(['fernanpop/error/'], {
+            state: {
+              message: 'Parece que el producto no se encuentra'
+            }
+          });
+        }
+      },
+    });
 
     this.form = this.formBuilder.group(
       {
@@ -93,46 +104,57 @@ export class UpdateProductComponent implements OnInit {
     );
   }
 
+  ngOnDestroy(): void {
+    this.getProductsByIdSubscription.unsubscribe();
+    this.updateProductSubscription.unsubscribe();
+    this.deleteProductSubscription.unsubscribe();
+  }
+
   // Obtenemos un campo del formulario
   get f(): { [key: string]: AbstractControl } {
     return this.form.controls;
   }
 
   onSubmit(): void {
-    console.log('submit');
     this.submitted = true;
 
-    console.log(this.form.invalid)
     if (this.form.invalid) {
       return;
     }
 
+    this.isLoading = true;
+    this.updateProductState.set(new LoadingState());
+
     const { title, price, img, desc } = this.form.value;
 
+    const { id, sellerId, status, createdAt } = this.productState().data;
+
     const updatedProduct: Product = {
-      id: this.product()!.id,
-      sellerId: this.product()!.sellerId,
+      id: id,
+      sellerId: sellerId,
       title: title,
       price: price,
       desc: desc,
       img: img,
-      status: this.product()!.status,
-      createdAt: this.product()!.createdAt,
-      updatedAt: this.product()!.updatedAt
+      status: status,
+      createdAt: createdAt,
+      updatedAt: new Date()
     }
 
-    this.productsService.updateProduct(this.currentUser()!.accessToken, updatedProduct).subscribe((result: Product | null) => {
-      if (!result) {
-        // Redirección a error con mensaje
-        this.router.navigate(['fernanpop/error/'], {
-          state: {
-            message: 'Parece que no se pudo modificar el producto'
-          }
-        });
-      } else {
-        this.router.navigate(['/fernanpop/product', result.id]);
-      }
-    });
+    //!TODO
+    // this.updateProductSubscription = this.productsService.updateProduct(this.currentUser()!.accessToken, updatedProduct).subscribe({
+    //   next: (response: CustomResponse) => {
+    //     if (response instanceof SuccessResponse) {
+    //       this.router.navigate(['/fernanpop/product', response.data.id]);
+    //       return;
+    //     } 
+    //     this.router.navigate(['fernanpop/error/'], {
+    //       state: {
+    //         message: 'Parece que no se pudo modificar el producto'
+    //       }
+    //     });
+    //   },
+    // });
   }
 
   onDelete(event: Event): void {
@@ -151,19 +173,22 @@ export class UpdateProductComponent implements OnInit {
         // No hacemos nada
       },
       accept: () => {
+        this.isLoading = true;
+        this.deleteProductState.set(new LoadingState());
+        //!TODO
         // Borramos el producto
-        this.productsService.deleteProduct(this.currentUser()!.accessToken, this.product()!.id).subscribe((result: Product | null) => {
-          if (result) {
-            this.router.navigate(['']);
-          } else {
-            // Redirección a error con mensaje
-            this.router.navigate(['fernanpop/error/'], {
-              state: {
-                message: 'Parece que no se pudo borrar el producto'
-              }
-            });
-          }
-        });
+        // this.productsService.deleteProduct(this.currentUser()!.accessToken, this.productState()!.id).subscribe((result: Product | null) => {
+        //   if (result) {
+        //     this.router.navigate(['']);
+        //   } else {
+        //     // Redirección a error con mensaje
+        //     this.router.navigate(['fernanpop/error/'], {
+        //       state: {
+        //         message: 'Parece que no se pudo borrar el producto'
+        //       }
+        //     });
+        //   }
+        // });
       }
     });
   }
